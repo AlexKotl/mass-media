17388cd92ec2467bfaffdbdcf81c43443ee723dd
Alex <slicer256@gmail.com>
2017-10-22 23:58:56 +0300

    added basic description page

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -3,13 +3,27 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Site;
 
 class SiteController extends Controller
 {
-    public function detailsAction()
+    public function detailsAction($url)
     {
+        $result = $this->getDoctrine()->getRepository(Site::class)
+            ->findBy(['url' => $url]);
+
+        if (count($result) === 0) {
+            throw $this->createNotFoundException('The site is not exist in database');
+        }
+
+        $site = $result[0];
+
+        if ($site->getFlag() !== 1) {
+            throw $this->createNotFoundException('The site is blocked');
+        }
+
         return $this->render('AppBundle:Site:details.html.twig', array(
-            // ...
+            'site' => $site
         ));
     }
 
