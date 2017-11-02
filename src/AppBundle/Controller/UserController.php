2ddccd1b678d61a6870caa654e92486d06b64c1f
Alex <slicer256@gmail.com>
2017-11-02 23:52:54 +0200

    display avatara from database

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
 
 class UserController extends Controller
 {
@@ -13,6 +15,18 @@ class UserController extends Controller
         return $this->render('AppBundle:Home:index.html.twig', [
             //'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    public function avataraAction($id) {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+
+        return new Response(
+            stream_get_contents($user->getAvatara()),
+            Response::HTTP_OK,
+            array(
+                'Content-Type' => 'application/octet-stream'
+            )
+        );
     }
 
 }
